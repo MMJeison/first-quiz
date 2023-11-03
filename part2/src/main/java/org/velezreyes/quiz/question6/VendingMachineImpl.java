611b27a68d55c97e8c358dfbf96dcbfded8a3fdd
Jeison Moncayo <desarrollo5ude@udea.edu.co>
2023-11-03 00:56:11 -0500

    Completado part2

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,47 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+import java.util.HashMap;
+
+public class VendingMachineImpl implements VendingMachine {
+
+  private int quarters = 0; // 25 cents
+
+  private static VendingMachineImpl instance;
+
+  private VendingMachineImpl() {
+  }
 
   public static VendingMachine getInstance() {
-    // Fix me!
-    return null;
+    if (instance == null) {
+      instance = new VendingMachineImpl();
+    }
+    return instance;
+  }
+
+  @Override
+  public void insertQuarter() {
+    quarters++;
+  }
+
+  @Override
+  public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+    if (quarters == 0) {
+      throw new NotEnoughMoneyException();
+    }
+    if (name.equals("ScottCola")) {
+      if (quarters < 3) {
+        throw new NotEnoughMoneyException();
+      }
+      quarters -= 3;
+      return new DrinkImpl(name, true);
+    }
+    if (name.equals("KarenTea")) {
+      if (quarters < 4) {
+        throw new NotEnoughMoneyException();
+      }
+      quarters -= 4;
+      return new DrinkImpl(name, false);
+    }
+    throw new UnknownDrinkException();
   }
 }
